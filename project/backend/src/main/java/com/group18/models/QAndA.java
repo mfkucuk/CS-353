bfdf210fe40daebf053f2b0532e15d155f1149ff
mfkucuk <mfkucuk555@gmail.com>
2023-06-02 01:36:11 +0300

    Create QAndA.java

---
+++ project/backend/src/main/java/com/group18/models/QAndA.java
@@ -0,0 +1,37 @@
+package com.group18.models;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class QAndA 
+{
+    private UUID askId;
+    private UUID answerId;
+    private UUID rentalId;
+    private LocalDateTime askDate;
+    private LocalDateTime answerDate;
+    private String question;
+    private String answer;
+
+    public QAndA(
+        UUID askId,
+        UUID answerId,
+        UUID rentalId,
+        LocalDateTime askDate,
+        LocalDateTime answerDate,
+        String question,
+        String answer
+    )
+    {
+        this.askId = askId;
+        this.answerId = answerId;
+        this.rentalId = rentalId;
+        this.askDate = askDate;
+        this.answerDate = answerDate;
+        this.question = question;
+        this.answer = answer;
+    }
+}
