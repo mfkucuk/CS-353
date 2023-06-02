387bd63f0455b3228cf3d795e3ca6622bfb46b68
denizcbilkent <denizc@ug.bilkent.edu.tr>
2023-06-03 02:19:35 +0300

    Update TravelerRental.js

--- project/frontend/src/Pages/Traveler/TravelerRental.js
+++ project/frontend/src/Pages/Traveler/TravelerRental.js
@@ -28,6 +28,37 @@ const contentStyle = {
     height: 'auto',
   };
 
+  const openAnswerWindowButtonStyle = {
+    backgroundColor: '#ffbd59',
+    color: '#fff',
+    padding: '8px 16px',
+    borderRadius: '4px',
+    border: 'none',
+    cursor: 'pointer',
+    marginTop: '8px',
+  };
+  
+const answerInputContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '10px',
+  };
+  
+  const answerInputStyle = {
+    width: '300px',
+    marginRight: '10px',
+  };
+  
+  const answerButtonStyle = {
+    padding: '5px 10px',
+    backgroundColor: '#FFBD59',
+    border: 'none',
+    borderRadius: '5px',
+    color: 'white',
+    cursor: 'pointer',
+  };
+
   const homeownerStyle = {
     display: 'flex',
     alignItems: 'center',
@@ -67,6 +98,62 @@ const contentStyle = {
     borderRadius: '34px',
   };
 
+  const answerStyle = {
+    marginLeft: '20px', // Indent the answer
+    marginBottom: '10px',
+    marginTop: '15px',
+    padding: '10px',
+    border: '1px solid #FFBD59',
+    borderRadius: '5px',
+    wordWrap: 'break-word', // Break long words into multiple lines
+  };
+  
+
+  const questionSectionStyle = {
+    width: '80vw',
+    margin: '20px auto',
+    border: '1px solid #FFBD59',
+    borderRadius: '5px',
+    padding: '10px',
+    alignContent: 'center',
+  };
+  
+  const questionStyle = {
+    marginBottom: '10px',
+    padding: '10px',
+    border: '1px solid #FFBD59',
+    borderRadius: '5px',
+    wordWrap: 'break-word', // Break long words into multiple lines
+  };
+  
+  const questionInputContainerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end', // Align items to the right
+    marginTop: '20px',
+  };
+  
+  const questionInputStyle = {
+    width: '30vw',
+    padding: '10px',
+    border: '1px solid #FFBD59',
+    borderRadius: '5px',
+    marginRight: '10px',
+    boxSizing: 'border-box',
+  };
+  
+  const questionButtonStyle = {
+    backgroundColor: '#FFBD59',
+    color: '#4B0082',
+    padding: '10px 20px',
+    fontWeight: 'bold',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    fontSize: '16px',
+    alignSelf: 'flex-start', // Align button to the top
+  };
+
   const switchLabelStyle = {
     marginLeft: '20px',
     marginRight: '20px',
@@ -89,8 +176,7 @@ const contentStyle = {
     padding: '10px',
     border: '1px solid #FFBD59',
     borderRadius: '5px',
-    display: 'flex',
-    width: 'fit-content', // Set width to fit the content
+    wordWrap: 'break-word', // Break long words into multiple lines
   };
 
   const commentInputContainerStyle = {
@@ -145,7 +231,16 @@ const TravelerRental = () => {
   const [showComments, setShowComments] = useState(true);
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState('');
+  const [questions, setQuestions] = useState([]);
+  const [newQuestion, setNewQuestion] = useState('');
+  const [answers, setAnswers] = useState([]);
+  const [answerInputs, setAnswerInputs] = useState([]);
+  const [selectedQuestionIndex, setSelectedQuestionIndex] = useState(null);
 
+
+  // ...
+
+ 
   /*useEffect(() => {
       const fetchRentalData = async () => {
           try {
@@ -177,6 +272,28 @@ const TravelerRental = () => {
     transform: showComments ? 'translateX(0)' : 'translateX(26px)',
   };
 
+
+  // ...
+
+  const handleQuestionChange = (event) => {
+    setNewQuestion(event.target.value);
+  };
+
+  
+  const handleAddQuestion = () => {
+    setQuestions((prevQuestions) => [...prevQuestions, newQuestion]);
+    setAnswers((prevAnswers) => [...prevAnswers, '']);
+    setAnswerInputs((prevInputs) => [...prevInputs, '']);
+    setNewQuestion('');
+  };
+
+  const handleAnswerInputChange = (index, event) => {
+    setAnswerInputs((prevInputs) => {
+      const updatedInputs = [...prevInputs];
+      updatedInputs[index] = event.target.value;
+      return updatedInputs;
+    });
+  };
   const handleSwitchComments = () => {
     setShowComments((prevState) => !prevState);
   };
@@ -191,90 +308,162 @@ const TravelerRental = () => {
     // Clear the text field
     setNewComment('');
   };
+  
+  const handleAnswer = (index) => {
+    const answer = answerInputs[index];
+    if (answer) {
+      handleAddAnswer(index, answer);
+      setAnswerInputs((prevInputs) => {
+        const updatedInputs = [...prevInputs];
+        updatedInputs[index] = '';
+        return updatedInputs;
+      });
+      setSelectedQuestionIndex(null); // Close the mini window after submitting the answer
+    }
+  };
 
+  const handleOpenAnswerWindow = (index) => {
+    setSelectedQuestionIndex(index);
+  };
+
+  const handleAddAnswer = (questionIndex, answer) => {
+    setAnswers((prevAnswers) => {
+      const updatedAnswers = [...prevAnswers];
+      updatedAnswers[questionIndex] = answer;
+      return updatedAnswers;
+    });
+  };
+  
   return (
-    <div
-      style={{
-        backgroundColor: '#4b0082',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: 'white',
-        overflowY: 'auto',
-      }}
-    >
-      <div style={{ position: 'absolute', top: 0, left: 0 }}>
-        <img src="/bilkent_logo.png" alt="Logo" style={{ margin: 50, width: 250, height: 100 }} />
-      </div>
-      <h1 style={{ color: '#FFBD59' }}>Rental in {rentalData.location}</h1>
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%', marginTop: 20 }}>
-        <div>
-          <img src="/example_rental.jpg" alt="Rental" style={imageStyle} />
-        </div>
-        <div>
-          <p style={contentStyle}>Location: {rentalData.location}</p>
-          <p style={contentStyle}>Room Count: {rentalData.roomCount}</p>
-          <p style={contentStyle}>Rating: {rentalData.rating}/5</p>
-          <p style={contentStyle}>
-            Available dates: {rentalData.startDate} to {rentalData.endDate}
-          </p>
-          <p style={contentStyle}>Features: {rentalData.features}</p>
-          <p style={contentStyle}>Restrictions: {rentalData.restrictions}</p>
-          <p style={contentStyle}>Rental Type: {rentalData.rentalType}</p>
-          <p style={contentStyle}>Price: {rentalData.price}</p>
-          <div style={homeownerStyle}>
-            <img src="/default_pp.png" alt="Homeowner" style={homeownerImageStyle} />
-            <p style={contentStyle}>{rentalData.homeownerName}</p>
-            <button style={buttonStyle}>Ask Question</button>
-          </div>
-        </div>
-      </div>
-      <button style={buttonStyle}>Rent</button>
-      <div style={switchContainerStyle}>
-        <span style={switchLabelStyle}>Comments</span>
-        <label className="switch" style={switchButtonStyle}>
-          <input type="checkbox" checked={showComments} onChange={handleSwitchComments} />
-          <span className="slider" style={switchSliderStyle}>
-            <span className="slider-before" style={switchSliderBeforeStyle}></span>
-          </span>
-        </label>
-        <span style={switchLabelStyle}>Questions</span>
-      </div>
-      {showComments ? (
-        <div style={commentSectionStyle}>
-        <h2 style={{ textAlign: 'center', color: '#ffbd59' }}>Comments</h2>
-        <div style={commentSectionScrollStyle}>
-          <div style={{commentSectionScrollInnerStyle}}>
-            {comments.map((comment, index) => (
-            <div key={index} style={commentStyle}>
-              {comment}
+    <div>
+        <div style={{width: '100%', height: '170px', backgroundColor: '#4b0082' }}>
+            <div style={{ position: 'absolute', top: 0, left: 0 }}>
+                <img src="/bilkent_logo.png" alt="Logo" style={{ margin: 50, width: 250, height: 100 }} />
             </div>
-          ))}
-          </div>
+        </div>
+        <div
+        style={{
+            backgroundColor: '#4b0082',
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white',
+            overflowY: 'auto',
+        }}
+        >
+        
+        <h1 style={{ color: '#FFBD59' }}>Rental in {rentalData.location}</h1>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%', marginTop: 20 }}>
+            <div>
+            <img src="/example_rental.jpg" alt="Rental" style={imageStyle} />
+            </div>
+            <div>
+            <p style={contentStyle}>Location: {rentalData.location}</p>
+            <p style={contentStyle}>Room Count: {rentalData.roomCount}</p>
+            <p style={contentStyle}>Rating: {rentalData.rating}/5</p>
+            <p style={contentStyle}>
+                Available dates: {rentalData.startDate} to {rentalData.endDate}
+            </p>
+            <p style={contentStyle}>Features: {rentalData.features}</p>
+            <p style={contentStyle}>Restrictions: {rentalData.restrictions}</p>
+            <p style={contentStyle}>Rental Type: {rentalData.rentalType}</p>
+            <p style={contentStyle}>Price: {rentalData.price}</p>
+            <div style={homeownerStyle}>
+                <img src="/default_pp.png" alt="Homeowner" style={homeownerImageStyle} />
+                <p style={contentStyle}>{rentalData.homeownerName}</p>
+                <button style={buttonStyle}>Ask Question</button>
+            </div>
+            </div>
+        </div>
+        <button style={buttonStyle}>Rent</button>
+        <div style={switchContainerStyle}>
+            <span style={switchLabelStyle}>Comments</span>
+            <label className="switch" style={switchButtonStyle}>
+            <input type="checkbox" checked={showComments} onChange={handleSwitchComments} />
+            <span className="slider" style={switchSliderStyle}>
+                <span className="slider-before" style={switchSliderBeforeStyle}></span>
+            </span>
+            </label>
+            <span style={switchLabelStyle}>Questions</span>
+        </div>
+        {showComments ? (
+            <div style={commentSectionStyle}>
+            <h2 style={{ textAlign: 'center', color: '#ffbd59' }}>Comments</h2>
+            <div style={commentSectionScrollStyle}>
+            <div style={{commentSectionScrollInnerStyle}}>
+                {comments.map((comment, index) => (
+                <div key={index} style={commentStyle}>
+                {comment}
+                </div>
+            ))}
+            </div>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+            <div style={commentInputContainerStyle}>
+                <input
+                type="text"
+                value={newComment}
+                onChange={handleCommentChange}
+                style={commentInputStyle}
+                placeholder="Add a comment"
+                />
+                <button style={commentButtonStyle} onClick={handleAddComment}>
+                Comment
+                </button>
+            </div>
+            </div>
+        </div>
+        ) : (
+            <div style={questionSectionStyle}>
+        <h2 style={{ textAlign: 'center', color: '#ffbd59' }}>Questions</h2>
+        <div style={commentSectionScrollStyle}>
+            <div style={commentSectionScrollInnerStyle}>
+            {questions.map((question, index) => (
+                <div key={index} style={questionStyle}>
+                {question}
+                {selectedQuestionIndex === index && (
+                    <div style={answerInputContainerStyle}>
+                    <input
+                        type="text"
+                        value={answerInputs[index]}
+                        onChange={(event) => handleAnswerInputChange(index, event)}
+                        style={questionInputStyle}
+                        placeholder="Your answer"
+                    />
+                    <button style={questionButtonStyle} onClick={() => handleAnswer(index)}>
+                        Reply
+                    </button>
+                    </div>
+                )}
+                {!selectedQuestionIndex && !answers[index] && (
+                    <button style={buttonStyle} onClick={() => handleOpenAnswerWindow(index)}>
+                    Answer
+                    </button>
+                )}
+                {answers[index] && <div style={answerStyle}>{answers[index]}</div>}
+                </div>
+            ))}
+            </div>
         </div>
         <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
-          <div style={commentInputContainerStyle}>
+            <div style={questionInputContainerStyle}>
             <input
-              type="text"
-              value={newComment}
-              onChange={handleCommentChange}
-              style={commentInputStyle}
-              placeholder="Add a comment"
+                type="text"
+                value={newQuestion}
+                onChange={handleQuestionChange}
+                style={questionInputStyle}
+                placeholder="Ask a question"
             />
-            <button style={commentButtonStyle} onClick={handleAddComment}>
-              Comment
+            <button style={questionButtonStyle} onClick={handleAddQuestion}>
+                Ask
             </button>
-          </div>
+            </div>
         </div>
-      </div>
-      ) : (
-        <div>
-          <h2 style={{ color: '#ffbd59' }}>Questions</h2>
-          {/* Question section code goes here */}
         </div>
-      )}
+        )}
+        </div>
     </div>
   );
 };
